dc1e6ad01fe3b35eceeb36df000e9ca25988c691
Alex <1987lex@gmail.com>
2025-11-29 14:55:05 +0100

    update requirements

--- main_dev.py
+++ main_dev.py
@@ -1,11 +1,17 @@
 import services.alerts_api
 import asyncio
-from config import settings
+from config import settings, regions
+from icecream import ic
 async def main():
     alerts_api_service = services.alerts_api.AlertsApiService()
     r = await alerts_api_service.get_alerts_status()
-    print(r)
+    # `ic(r[''])
 
+    for k in r:
+        ic(k)
+    print(r.regions)
+    print(r.regions['Автономна Республіка Крим'])
 if __name__ == "__main__":
     asyncio.run(main())
-    print(settings.alerts_api_token)
+    ic(settings.alerts_api_token)
+    # ic()
